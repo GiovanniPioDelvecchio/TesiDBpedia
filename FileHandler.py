328898707daa214120296748c71d2c451b614443
unknown <giovannidelvecchio4@gmail.com>
2021-05-06 11:17:02 +0200

    Aggiunta la documentazione per il file EntityHandler

--- FileHandler.py
+++ FileHandler.py
@@ -4,8 +4,26 @@ import string
 import random as rng
 import json
 
+"""Package che si occupa di gestire i files utilizzati all'interno del progetto
+
+L'obiettivo di questo package è esporre un'interfaccia con i files agli altri moduli, in modo
+da non demandare ai singoli moduli l'accesso alle risorse esterne.
+
+Esempio tipico di utilizzo:
+
+foo = get_uri_from_file('files_to_add\entities_new.txt')
+bar = expand_entity_file('files_to_add\entities_new.txt')
+"""
+
 
 def get_uri_from_file(file_name):
+    """Considera un'entità a caso presa dal file file_name
+
+    Args:
+        file_name: file di entità da cui bisogna prenderne una.
+    Returns:
+         entity_list[n_result]: una delle entry del file, precisamente la entry in posizione n_result.
+    """
     entities_file = open(file_name, 'r', encoding="utf-8")
     entities = entities_file.read()
     entity_list = entities.split("\n")
@@ -23,8 +41,12 @@ def get_uri_from_file(file_name):
 # -avere il prefisso dbr:
 # -avere almeno 3 tipi con prefisso dbo:
 def expand_entity_file(file_name):
+    """Funzione che si occupa di espandere (riscrivere) il file di entità, in base alle caratteristiche scelte.
+
+    Args:
+        file_name: file di entità da arricchire con nuove entità che soddisfino determinate condizioni.
+    """
     alphabet = list(string.ascii_uppercase)
-    # character = alphabet[0]
     max_len = 100
     entities_file = open(file_name, 'w', encoding="utf-8")
     for character in alphabet:
@@ -39,7 +61,6 @@ def expand_entity_file(file_name):
                 HAVING (COUNT(?t)>=3)
                 LIMIT """ + str(max_len) + """ 
                 """
-
         print(query)
         sparql = SPARQLWrapper("http://dbpedia.org/sparql")
         sparql.addExtraURITag("timeout", "30000")
@@ -55,6 +76,13 @@ def expand_entity_file(file_name):
 
 
 def get_type_blacklist(file_name="files_to_add/type_blacklist.txt"):
+    """Si occupa di prendere da file la lista completa di tipi di entità da escludere.
+
+    Args:
+        file_name: file da cui prendere la lista di URI pari ai tipi di entità da escludere.
+    Returns:
+        types_list: lista di URI pari ai tipi di entità da escludere.
+    """
     type_file = open(file_name, 'r', encoding="utf-8")
     types_read = type_file.read()
     types_list = types_read.split("\n")
@@ -62,6 +90,13 @@ def get_type_blacklist(file_name="files_to_add/type_blacklist.txt"):
 
 
 def get_relation_whitelist(file_name="files_to_add/relations_new.txt"):
+    """Si occupa di prendere da file la lista completa di relazioni da considerare.
+
+    Args:
+        file_name: file da cui prendere la lista di URI pari alle relazioni da considerare.
+    Returns:
+        relation_list: lista di URI pari alle relazioni da considerare.
+    """
     relations_file = open(file_name, 'r', encoding="utf-8")
     relation_read = relations_file.read()
     relation_list = relation_read.split("\n")
@@ -69,6 +104,12 @@ def get_relation_whitelist(file_name="files_to_add/relations_new.txt"):
 
 
 def remove_relation_from_whitelist(relation_name, file_name="files_to_add/relations_new.txt"):
+    """Si occupa di eliminare dal file file_name l'URI relation_name
+
+    Args:
+        relation_name: URI della relazione da cancellare dal file file_name
+        file_name: file da cui cancellare l'URI relation_name (se presente, altrimenti il file rimane invariato).
+    """
     relations_file = open(file_name, 'r', encoding="utf-8")
     relation_read = relations_file.read()
     relation_list = relation_read.split("\n")
@@ -80,6 +121,13 @@ def remove_relation_from_whitelist(relation_name, file_name="files_to_add/relati
 
 
 def serialize_query_set(dict_to_serialize, file_name="files_to_add/generated_queries.txt"):
+    """Si occupa di serializzare un dizionario all'interno del file file_name, utilizzando la sintassi JSON con
+    una certa spaziatura ed indentazione.
+
+    Args:
+        dict_to_serialize: dizionario da serializzare su file file_name.
+        file_name: file su cui va serializzato dict_to_serialize con sintassi JSON.
+    """
     generated_file = open(file_name, 'w', encoding="utf-8")
     generated_file.write(json.dumps(dict_to_serialize, indent = 4, sort_keys = True))
 
